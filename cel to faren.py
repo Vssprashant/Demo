c0a7c2bebd10db1ce9ae1519eb1825a9aa272bc7
Vssprashant <74755803+Vssprashant@users.noreply.github.com>
2022-03-30 12:30:27 +0530

    Update cel to faren.py

--- cel to faren.py
+++ cel to faren.py
@@ -6,7 +6,7 @@ print('The %.2f degree Celsius is equal to: %.2f Fahrenheit'
       %(celsius_1, Fahrenheit_1))  
   
 print("----OR----")  
-celsius_2 = float (input("Temperature value in degree Celsius: " ))  
+celsius_2 = 33  
 Fahrenheit_2 = (celsius_2 * 9/5) + 32  
   
 print ('The %.2f degree Celsius is equal to: %.2f Fahrenheit'  
